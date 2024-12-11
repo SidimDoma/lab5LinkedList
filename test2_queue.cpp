982356d2bc9bb5102e40321a81e9a996bf6d2d28
unknown <timzakharow@gmail.com>
2024-12-11 20:41:08 +0300

    7 Task change

--- test2_queue.cpp
+++ test2_queue.cpp
@@ -17,16 +17,14 @@ private:
 template <class T, class C> T sum (const C& c)
 {
     T res = 0;
-    for (typename C::ConstIterator i = c.cbegin(); i != c.cend(); ++i){
-       res += *i;}
+    for (typename C::ConstIterator i = c.cbegin(); i != c.cend(); ++i)
+       res += *i;
     return res;
 }
 
 int main()
 {
-    Stack<int> s1, s2;
-
-
+    Queue<int> s1, s2;
     numerate<int> f(100);
 
     s1.Push(1);
@@ -37,18 +35,18 @@ int main()
     s2 = s1;
     s2.Push(4);
 
-    cout << s1 << endl;          // 3->2->1
+    cout << s1 << endl;          // 1->2->3
     cout << s1.Size() << endl;
 
-    cout << s2 << endl;          // 4->3->2->1
+    cout << s2 << endl;          // 1->2->3->4
     cout << s2.Size() << endl;
 
     swap(s1, s2);
 
-    cout << s1 << endl;          // 4->3->2->1
+    cout << s1 << endl;          // 1->2->3->4
     cout << s1.Size() << endl;
 
-    cout << s2 << endl;          // 3->2->1
+    cout << s2 << endl;          // 1->2->3
     cout << s2.Size() << endl;
 
     generate(s1.begin(), s1.end(), f);
@@ -56,9 +54,7 @@ int main()
     cout << s1 << endl;          // 100->101->102->103
     cout << s1.Size() << endl;
 
-    cout << sum<int, LinkedList<int>>(s2) << endl;     // 6
+    cout << sum<int,LinkedList<int>>(s2) << endl;     // 6
 
     return 0;
 }
-
-
